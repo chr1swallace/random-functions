a78ce10d688ff2318cb50e81addfd94e6e0301fa
Chris Wallace <chris.wallace@cimr.cam.ac.uk>
2013-05-21 15:25:39 +0200

    Create getArgs.R
    A function to parse commandArgs() in R, making Rscripts easy.

---
+++ getArgs.R
@@ -0,0 +1,51 @@
+##' commandArgs parsing
+##' 
+##' return a named list of command line arguments
+##'
+##' Usage:
+##' call the R script thus
+##'   ./myfile.R --args myarg=something
+##' or
+##'   R CMD BATCH --args myarg=something myfile.R
+##'
+##' Then in R do
+##'   myargs <- getArgs()
+##' and myargs will be a named list
+##' > str(myargs)
+##' List of 2
+##' $ file : chr "myfile.R"
+##' $ myarg: chr "something"
+##'
+##' @title getArgs
+##' @param verbose print verbage to screen 
+##' @param defaults a named list of defaults, optional
+##' @return a named list
+##' @author Chris Wallace
+getArgs <- function(verbose=FALSE, defaults=NULL) {
+  myargs <- gsub("^--","",commandArgs(TRUE))
+  setopts <- !grepl("=",myargs)
+  if(any(setopts))
+    myargs[setopts] <- paste(myargs[setopts],"=notset",sep="")
+  myargs.list <- strsplit(myargs,"=")
+  myargs <- lapply(myargs.list,"[[",2 )
+  names(myargs) <- lapply(myargs.list, "[[", 1)
+
+  ## logicals
+  if(any(setopts))
+    myargs[setopts] <- TRUE
+
+  ## defaults
+  if(!is.null(defaults)) {
+    defs.needed <- setdiff(names(defaults), names(myargs))
+    if(length(defs.needed)) {
+      myargs[ defs.needed ] <- defaults[ defs.needed ]
+    }
+  }
+
+  ## verbage
+  if(verbose) {
+    cat("read",length(myargs),"named args:\n")
+    print(myargs)
+  }
+  myargs
+}
