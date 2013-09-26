87df1bb52ad70fbe219d2844f71c3135a7a49c5b
chr1swallace <chris.wallace@cimr.cam.ac.uk>
2013-09-26 16:06:46 +0100

    made it a proper package

---
+++ R/random-functions-package.R
@@ -0,0 +1,11 @@
+#'title randomFunctions
+#'@name random-functions-package
+#'@docType package
+#'@author Chris Wallace <chris.wallace@@cimr.cam.ac.uk>
+#'@keywords package
+#'@import ggplot2 
+#'@importFrom ggdendro dendro_data
+#'@importFrom reshape melt
+#'@import grid
+NULL
+
