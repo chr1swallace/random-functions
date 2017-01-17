fdba72de44cb6a1765c8efa3ff53de83b8ccaa3f
chr1swallace <cew54@cam.ac.uk>
2017-01-17 09:54:20 +0000

    added verbose to textvenn

--- R/textvenn.R
+++ R/textvenn.R
@@ -3,21 +3,26 @@
 ##' @title textvenn
 ##' @param A vector
 ##' @param B vector
-##' @param quiet if FALSE (default) report to screen
-##' @return invisibly returns a list containing items unique to A, the intersection, and items unique to B
+##' @param verbose if TRUE print all items to screen.  Default is FALSE.
+##' @param quiet if FALSE (default) report summary counts to screen
+##' @return invisibly returns a list containing items unique to A, the
+##'     intersection, and items unique to B
 ##' @export
 ##' @author Chris Wallace
-textvenn <- function(A,B,quiet=FALSE) {
+textvenn <- function(A,B,verbose=FALSE,quiet=FALSE) {
   AnotB <- setdiff(A,B)
   AandB <- intersect(B,A)
   BnotA <- setdiff(B,A)
   if(!quiet) {
-    cat("set A:\n")
-    cat(AnotB,"\n")
-    cat("intersection:\n")
-    cat(AandB,"\n")
-    cat("set B:\n")
-    cat(BnotA,"\n")
+    cat("set A:\t",length(AnotB),"\n")
+    if(verbose)
+        cat(AnotB,"\n")
+    cat("intersection:\t",length(AandB),"\n")
+    if(verbose)
+        cat(AandB,"\n")
+    cat("set B:\t",length(BnotB),"\n")
+    if(verbose)
+        cat(BnotA,"\n")
   }
   invisible(list(A=AnotB,int=AandB,B=BnotA))
 }
